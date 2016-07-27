a12a67f8f1f2f58910d3e59ce1b6dca05e60254d
TEAMvirus <virusteam@doctor.com>
2016-07-27 06:07:12 -0700

    Update Signal_bot.lua

--- bot/Signal_bot.lua
+++ bot/Signal_bot.lua
@@ -274,28 +274,19 @@ function create_config( )
     "Caption",
     "Payamresan"
     },
-    sudo_users = {175636120,214795464,198794027,180252366},
+   sudo_users = {167288719,174410791},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[👑 Signal Bot 👑
-An advanced administration bot based on TG-CLI written in Lua
-
-🔰 Github 🔰
-https://github.com/SIGNALTEAM/SignalTG.git
-
-Admins:
-🔥 @Ww_Developer_wW [Founder & Developer] 🔥
-
-🔥 @Mr_Hidden [Sudo & Manager] 🔥
-
-🔥 @SigNal_Founder [Sudo] 🔥
-
-🔥 @Im_Best_Sudo [Sudo] 🔥
-
-Special thanks to
-SEEDTEAM
-
-Our channels
-@Signal_CH
+    about_text = [[🤖 TeleAghrab 🤖�
+📛 @KING_OF_AQRAB [FoundeR]
+📛 @Ww_reza_aqrab_reza_wW [ManageR]
+ADD.GiTHuP 
+https://github.com/TEAMvirus/TeleAghrab.git
+Special thanks to 
+SeeDTeaM
+👾VirUS™ @vViruSs 
+⚜⚜⚜⚜⚜⚜⚜⚜
+Our ChannelS
+@Aqrab_team
 ]],
     help_text_realm = [[
 Realm Commands:
